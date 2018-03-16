61214e5f277de59b78945c4591c6d99eedc42c27
tizl1234 <tizldoma@outlook.com>
2018-03-16 22:25:11 +0300

    added new description

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,11 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
+//!
+//!прототип функции print для стандартного потока вывода
+//!
 void print(const std::string& text, std::ostream& out = std::cout);
+//!
+//!прототип функции print для вывода из файла
+//!
 void print(const std::string& text, std::ofstream& out);
